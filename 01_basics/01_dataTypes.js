c5bf714f5cf43b11e145d5a7d599d62397b6b3ee
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-10-11 11:27:01 +0000

    Overview of dataTypes in Js

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all Js code as new Version
+
+//number  =>  2 to power 53
+//bigInt => greater int value
+//boolean => true/false
+//string = ""
+//undefined => if varible is only defined with no value   | typeof(undefined) is undifined 
+//null => standalone value typeof(null) is object 
+//symbol => unique 
+
+
+//non-premitive
+
+//object
+
+
