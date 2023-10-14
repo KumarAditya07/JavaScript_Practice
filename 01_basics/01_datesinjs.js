a5280c8581641a8baaad8c79cb3fe03dde71b077
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-10-14 11:53:44 +0000

    date & time basics in js

---
+++ 01_basics/01_datesinjs.js
@@ -0,0 +1,12 @@
+let myDate = new Date();
+
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+
+
+
+console.log(myDate.getMonth()+1);
