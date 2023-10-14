84a1b4023740384922ec2840279023ab8976d153
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-10-14 11:12:49 +0000

    Basics of string im Js

---
+++ 01_basics/01string.js
@@ -0,0 +1,13 @@
+const name = "        Aditya       ";
+const repoCount = 22;
+
+// console.log(name + repoCount + " value"); ///not suggested method to concatinate instead 
+
+
+console.log(`Hello my name is ${name.trim()} and my github repoCount is ${repoCount}`);
+
+const gameName = new String('Kalita')
+
+console.log(gameName.indexOf('K'));
+console.log(`substring : ${gameName.substring(0,3)}`)
+console.log(`slice : ${gameName.slice(1,4)}`)
