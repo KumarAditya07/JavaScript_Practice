38d654df0c55588a0ee4f123f9f7ffa90d785b0a
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-10-19 11:37:42 +0000

    basics of Async JavaScript

---
+++ Async_javaScript_Basics/asyncc.js
@@ -0,0 +1,43 @@
+/*
+JavaScript  :  Saynchronous
+            :  Single threaded
+
+
+Excutopn context : execute one line of code at a time
+                 |: console log->1
+                 |: console log->2                   each excution wait for the last one to complete before exrcting
+                 |
+                 call stack     Memory Heap
+
+
+Blocking Code              vs                Non Blocking code
+    |                                               |
+Block the flow of the program                 Do not Block the flow of the program    
+    |                                                |
+Read file Sync                                 Read File Async
+
+
+// . JS asynchronous language h but uska default behaviour synchronous hota h. Us behavior ko hame forcefully bta kr async bnana pdta h. 
+Sbse easy example h aapke settimeout or setinterval methods..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
