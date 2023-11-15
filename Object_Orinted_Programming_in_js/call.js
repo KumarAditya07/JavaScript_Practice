8a4e87c13dd386422f258165c365202e1fea006e
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-15 13:41:08 +0000

    made some chnages

--- Object_Orinted_Programming_in_js/call.js
+++ Object_Orinted_Programming_in_js/call.js
@@ -8,8 +8,9 @@ function createUser(username, email, password){
     SetUsername.call(this,username)
    
     this.email = email;
+
     this.password = password;
 }
 
-const chai = new createUser("chai", "chai@fb.com", "123")
+const chai = new createUser("tea", "chai@fb.com", "123")
 console.log(chai);
