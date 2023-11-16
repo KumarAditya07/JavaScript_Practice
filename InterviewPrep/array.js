1e85db92f3dd10c03f71afdf56dd906162d3d032
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-16 16:22:56 +0000

    practcing some i/p o/p question on map/filter/reducer

--- InterviewPrep/array.js
+++ InterviewPrep/array.js
@@ -26,4 +26,25 @@ const sum  = nums.reduce((accumulator,curr,i,nums)=>{
      return accumulator + curr;
 },0);
 
-console.log(sum);
+// console.log(sum);
+
+// I/p & O/p based question
+
+let student = [
+    {name:"Aman",uid:123,marks:90},
+    {name:"Saurabh",uid:4123,marks:60},
+    {name:"Akshay",uid:1623,marks:70},
+    {name:"Aditya",uid:1423,marks:40},
+]
+// comvert name into uppercse
+// return details of those who get more then 60 marks
+
+const newStudent = student.map((stu)=>stu.name.toUpperCase());
+
+// console.log(newStudent);
+
+const moreMarks = student.filter(({marks})=>{       //we are destructring student here we can gernal use student then studen.marks
+    return marks>60;
+})
+
+console.log(moreMarks);
