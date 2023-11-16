227d7b34f4774629d8b6cb58ac7cc1a423d45429
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-16 13:45:39 +0000

    Brush up some filter concepts in js

--- InterviewPrep/array.js
+++ InterviewPrep/array.js
@@ -1,11 +1,21 @@
-const nums = [1,2,3,4,5];
 
+
+const nums = [1,2,3,4,5];
+//Map concept & deep copy 
 const replica = nums.map((n,i,nums)=>{
     return n*3
 })
 const rep = nums;
-rep[0]=999;
+// rep[0]=999; here its cchanging the orignal array as well to avoid we can use spread oprator
 
-console.log(replica);
-console.log(nums);
-console.log(rep);
+// console.log(replica);
+// console.log(nums);
+// console.log(rep);
+
+//Filter Concept in JS
+
+const greaterThanTwo = nums.filter((n,i,nums)=>{
+    return n>3
+});
+
+console.log(greaterThanTwo);
