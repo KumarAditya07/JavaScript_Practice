b63307a515b1c869f347fcb5e516fd38ac38f81c
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-21 07:04:10 +0000

    funcation in js

--- InterviewPrep/funcation.js
+++ InterviewPrep/funcation.js
@@ -1,1 +1,66 @@
-//funcation questin in javascript      
+//funcation questin in javascript   
+
+// The code you've provided seems to be an example of a self-invoking function or an immediately invoked function expression (IIFE).
+
+// (function test (a){
+//     a=1;
+//     function test(a){
+//         if(!a){
+//             a=20;
+//             console.log(a);
+//     }
+//         }
+        
+//         console.log(a);
+//     test(a);
+//     console.log(a);
+// })();
+
+//  v1 = {}
+// v2 = {
+//     key : "455"
+// }
+
+// v3 = {
+//     key : "25"
+// }
+
+// v1[v2] = 23;
+// console.log(v1);
+
+// function find()
+// {
+// return {
+// bar: "hey there"
+// };
+// }
+
+// function good()
+// {
+// return
+// {
+// console.log("sds");
+// };
+// }
+// console.log(good())
+// console.log(find())
+//O/P
+// undefined
+// { bar: 'hey there' }
+
+
+// function sample () {
+//     a = 20;
+//     var b = 30;
+//     }
+//     sample();
+//     console.log(a);
+//     console.log(b);
+
+var num = 8;
+ var num = 10;
+ 
+ console.log(num);
+   
+
+
