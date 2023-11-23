8e52a4f9a9fa841ff0ba6c686e0da8b00572b613
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-23 05:25:33 +0000

    interview question on closure and IIFE

--- InterviewPrep/closure.js
+++ InterviewPrep/closure.js
@@ -1,36 +1,53 @@
-function init() {
-    var name = "Mozilla"; // name is a local variable created by init
-    function displayName() {
-      // displayName() is the inner function, that forms the closure
-      console.log(name); // use variable declared in the parent function
+// function init() {
+//     var name = "Mozilla"; // name is a local variable created by init
+//     function displayName() {
+//       // displayName() is the inner function, that forms the closure
+//       console.log(name); // use variable declared in the parent function
+//     }
+//     displayName();
+//   }
+//   init();
+
+
+//   function makeFunc() {
+//     const name = "Mozilla";
+//     function displayName() {
+//       console.log(name);
+//     }
+//     return displayName;
+//   }
+  
+//   const myFunc = makeFunc();
+//   myFunc();
+
+  
+//   function makeAdder(x) {
+//     return function (y) {
+//       return x + y;
+//     };
+//   }
+  
+//   const add5 = makeAdder(5);
+//   const add10 = makeAdder(10);
+  
+//   console.log(add5(2)); // 7
+//   console.log(add10(2)); // 12
+
+
+//using IIFE
+  // for(var i = 0;i<3 ;i++){
+  //   ( function call (i){
+  //     setTimeout(function (){
+  //     console.log(i);
+  //   },i*1000);})(i);
+  // }
+
+// using closure
+  for(var i=0;i<3;i++){
+      function loo(i){
+      setTimeout(function (){
+        console.log(i);
+      },i*1000);
     }
-    displayName();
+    loo(i);
   }
-  init();
-
-
-  function makeFunc() {
-    const name = "Mozilla";
-    function displayName() {
-      console.log(name);
-    }
-    return displayName;
-  }
-  
-  const myFunc = makeFunc();
-  myFunc();
-
-  
-  function makeAdder(x) {
-    return function (y) {
-      return x + y;
-    };
-  }
-  
-  const add5 = makeAdder(5);
-  const add10 = makeAdder(10);
-  
-  console.log(add5(2)); // 7
-  console.log(add10(2)); // 12
-  
-  
--- InterviewPrep/funcation.js
+++ InterviewPrep/funcation.js
@@ -1,108 +1,108 @@
-//funcation questin in javascript   
+// funcation questin in javascript   
 
 // The code you've provided seems to be an example of a self-invoking function or an immediately invoked function expression (IIFE).
 
-// (function test (a){
-//     a=1;
-//     function test(a){
-//         if(!a){
-//             a=20;
-//             console.log(a);
-//     }
-//         }
+(function test (a){
+    a=1;
+    function test(a){
+        if(!a){
+            a=20;
+            console.log(a);
+    }
+        }
         
-//         console.log(a);
-//     test(a);
-//     console.log(a);
-// })();
+        console.log(a);
+    test(a);
+    console.log(a);
+})();
 
-//  v1 = {}
-// v2 = {
-//     key : "455"
-// }
+ v1 = {}
+v2 = {
+    key : "455"
+}
 
-// v3 = {
-//     key : "25"
-// }
+v3 = {
+    key : "25"
+}
 
-// v1[v2] = 23;
-// console.log(v1);
+v1[v2] = 23;
+console.log(v1);
 
-// function find()
-// {
-// return {
-// bar: "hey there"
-// };
-// }
+function find()
+{
+return {
+bar: "hey there"
+};
+}
 
-// function good()
-// {
-// return
-// {
-// console.log("sds");
-// };
-// }
-// console.log(good())
-// console.log(find())
-//O/P
-// undefined
-// { bar: 'hey there' }
+function good()
+{
+return
+{
+console.log("sds");
+};
+}
+console.log(good())
+console.log(find())
+O/P
+undefined
+{ bar: 'hey there' }
 
 
-// function sample () {
-//     a = 20;
-//     var b = 30;
-//     }
-//     sample();
-//     console.log(a);
-//     console.log(b);
+function sample () {
+    a = 20;
+    var b = 30;
+    }
+    sample();
+    console.log(a);
+    console.log(b);
 
-// var str = "vwzxt";
-// let count=0
-// const result = str.charCodeAt(0);
-// // for(let i = 1;i<str.length;i++){
-// //     if(str.charCodeAt(i)<str.charCodeAt(i-1))
-// //        {
-// //         count++;
-// //        }
-// // }
-// //we cannot use map direct in string becz it array method 
-// [...str].map((char,i)=>{
+var str = "vwzxt";
+let count=0
+const result = str.charCodeAt(0);
+// for(let i = 1;i<str.length;i++){
 //     if(str.charCodeAt(i)<str.charCodeAt(i-1))
 //        {
 //         count++;
 //        }
-// })
- 
-//  console.log(count);
- //
-
-//  function sum()
-// {
-//     // let a = 8;
-//     const b=2;
-//     var c=a+b;
 // }
-// console.log(a,b,c)
-// sum()
-// console.log(a,b,c)
-
-// let arr=[1,2,3,4,5]
-// console.log(arr[2], arr.length);
-// arr.length=0;
-// console.log(arr[2], arr.length);
-
-// for (var i = 0; i < 3; i++) 
-// {
-//   setTimeout(function() { alert(i); }, 1000 + i);
-// }
+//we cannot use map direct in string becz it array method 
+[...str].map((char,i)=>{
+    if(str.charCodeAt(i)<str.charCodeAt(i-1))
+       {
+        count++;
+       }
+})
  
-// for(let i = 0; i <5; i++)
-// {
-// 	setTimeout(()=>{
-// 		console.log(i);
-//     },0)
-// }  
+ console.log(count);
+ 
+
+ function sum()
+{
+    // let a = 8;
+    const b=2;
+    var c=a+b;
+}
+console.log(a,b,c)
+sum()
+console.log(a,b,c)
+
+let arr=[1,2,3,4,5]
+console.log(arr[2], arr.length);
+arr.length=0;
+console.log(arr[2], arr.length);
+
+for (var i = 0; i < 3; i++) 
+{
+  setTimeout(function() { alert(i); }, 1000 + i);
+}
+ 
+for(let i = 0; i <5; i++)
+{
+	setTimeout(()=>{
+		console.log(i);
+    },0)
+}  
 
 let count = 0;
 (function immediate() {
