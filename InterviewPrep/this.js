3bb8f5eb1e596c371a0cfe2d2a68f197193b17fe
Kumar Aditya <122738572+KumarAditya07@users.noreply.github.com>
2023-11-27 06:18:50 +0000

    this interview queston

--- InterviewPrep/this.js
+++ InterviewPrep/this.js
@@ -1,13 +1,42 @@
-let name = "Akshay";
+// let name = "Akshay";
 
-let user = {
-    name : "aditya",
-    age  : 25,
-     chiileObej : {
-        getDetail : ()=>{
-            console.log(this.name);
-        },
-     }
+// let user = {
+//     name : "aditya",
+//     age  : 25,
+//      chiileObej : {
+//         getDetail : ()=>{
+//             console.log(this.name);
+//         },
+//      }
+// }
+
+// user.chiileObej.getDetail();
+
+// class user {
+//     constructor(n){
+//         this.name = n;
+//     }
+
+//     getDetail=()=>{
+//         console.log(this.name);
+//     }
+// }
+
+// const u = new user("Saurab");
+// u.getDetail();
+
+let calculator = {
+    read(){
+    this.a = +prompt("a = ",0);
+    this.b  = +prompt("b = ",0);
+    },
+    sum(){
+        return this.a + this.b;
+    },
+    mul(){
+        return this.a * this.b;
+     },
 }
-
-user.chiileObej.getDetail();
+calculator.read();
+calculator.sum();
+calculator.mul();
